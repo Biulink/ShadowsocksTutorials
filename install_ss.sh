89cebad361b780802563b0595781b25ca1ee69fc
免费Shadowsocks账号 <37216613+Biulink@users.noreply.github.com>
2019-03-07 00:45:30 +0800

    Update install_ss.sh

--- install_ss.sh
+++ install_ss.sh
@@ -6,6 +6,7 @@ echo 'aes-256-cfb' > /root/.my-shadowsocks-encryption
 
 /usr/bin/ssserver -s ::0 -p `cat /root/.my-shadowsocks-port` -k `cat /root/.my-shadowsocks-password` -m `cat /root/.my-shadowsocks-encryption` --user nobody --workers 2 -d start
 printf "\n/usr/bin/ssserver -s ::0 -p \`cat /root/.my-shadowsocks-port\` -k \`cat /root/.my-shadowsocks-password\` -m \`cat /root/.my-shadowsocks-encryption\` --user nobody --workers 2 -d start\n\n" >> /etc/rc.d/rc.local
+chmod +x /etc/rc.d/rc.local
 
 MY_IP=$(curl http://myip.dnsomatic.com)
 printf "*****************************************\nShadowsocks服务器已经安装成功，账号如下：\n服务器：$MY_IP \n端口：`cat /root/.my-shadowsocks-port` \n密码：`cat /root/.my-shadowsocks-password` \n加密方式：`cat /root/.my-shadowsocks-encryption` \n*****************************************\n"
